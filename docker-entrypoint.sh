10daf7155105f0c72c0a03bc336347ea735fecd2
chabad360 <chabad360@gmail.com>
2019-01-25 18:18:03 +0000

    some syntax errors...

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ ! $MYSQL_ROOT_PASSWORD ]; then
+if [[ -z ${MYSQL_ROOT_PASSWORD} ]]; then
     echo "MySQL root password not set! Using default: \"123\""
     export MYSQL_ROOT_PASSWORD=123
 fi
 
-if [ ! $ADMIN_PASSWORD ]; then
+if [[ -z ${ADMIN_PASSWORD} ]]; then
     echo "Admin password not set! Using default: \"admin\""
     export ADMIN_PASSWORD=admin
 fi
 
-if [ ! $WEBSERVER_PORT ]; then
+if [[ -z ${WEBSERVER_PORT} ]]; then
     echo "Webserver port not set! Using default: \"8000\""
     export WEBSERVER_PORT=8000
 fi
@@ -28,16 +28,16 @@ function setup_config () {
     \"redis_queue\": \"redis://redis-queue:11000\",\n\
     \"redis_socketio\": \"redis://redis-socketio:12000\",\n\
     \"restart_supervisor_on_update\": false,\n\
-    \"root_password\": \"$MYSQL_ROOT_PASSWORD\",\n\
+    \"root_password\": \"${MYSQL_ROOT_PASSWORD}\",\n\
     \"serve_default_site\": true,\n\
     \"shallow_clone\": true,\n\
     \"socketio_port\": 9000,\n\
     \"update_bench_on_update\": true,\n\
-    \"webserver_port\": $WEBSERVER_PORT,\n\
-    \"admin_password\": \"$ADMIN_PASSWORD\"\n\
+    \"webserver_port\": ${WEBSERVER_PORT},\n\
+    \"admin_password\": \"${ADMIN_PASSWORD}\"\n\
     }") > /home/frappe/frappe-bench/Procfile_docker
 
-    cat <(echo "web: bench serve --port $WEBSERVER_PORT\n\
+    cat <(echo "web: bench serve --port ${WEBSERVER_PORT}\n\
     \n\
     socketio: /usr/bin/node apps/frappe/socketio.js\n\
     watch: bench watch\n\
